34d78421dd2bf87c8323699182550840aa4cc031
NoJe <nojy99wodnjs@gmail.com>
2022-05-02 21:54:44 +0900

    fix bugs and complete alarm function

--- src/timer/AlarmInterface.js
+++ src/timer/AlarmInterface.js
@@ -1,22 +1,26 @@
 import React from 'react';
 
+const dayString = [ 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+
 function AlarmInterface(props){
     const style = {
         display: props.style
     };
+
     const alarmList = props.alarmSet.map((alarm)=>{
         const amPm = alarm.timeSet.hour >= 12 ? 'PM': 'AM';
+
         return (
             <div key={alarm.id} className='alarm-container'>
                 <span className='alarm-info'>
-                    <span className='day'>{alarm.day}</span>
+                    <span className='day'>{dayString[alarm.day]}</span>
                     <span className='time'>
                         {alarm.timeSet.hour}:{alarm.timeSet.min}
                     </span>
                     {amPm}
                 </span>
                 <label className="alarm-onoff">
-                    <input type="checkbox"></input>
+                    <input type="checkbox" ref={props.refList[alarm.id]}></input>
                     <span className="slider round"></span>
                 </label>
             </div>
--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -1,10 +1,10 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import Menu from './Menu';
 import Interface from './Interface';
 import Controller from './Controller';
 import AlarmInterface from './AlarmInterface';
-import {useState} from "react";
 import useInterval from './UseInterval';
+
 
 const timeObject = {
     t: 0,
@@ -47,75 +47,20 @@ const btnList = [
     },
 ];
 
-// max lenght is 6
 const alarmList = [
     {
         id: 0,
-        day: ' monday',
+        day: 0,
         timeSet : {
             t: 27000,
             sec: 0,
             min: 30,
             hour: 18,
             isPossible: false,
-        }
+        },
     },
-    {
-        id: 1,
-        day: 'wednesday',
-        timeSet : {
-            t: 27000,
-            sec: 0,
-            min: 30,
-            hour: 20,
-            isPossible: false,
-        }
-    },
-    {
-        id: 2,
-        day: ' thursday',
-        timeSet : {
-            t: 27000,
-            sec: 0,
-            min: 20,
-            hour: 10,
-            isPossible: false,
-        }
-    },
-    {
-        id: 3,
-        day: ' monday',
-        timeSet : {
-            t: 27000,
-            sec: 0,
-            min: 30,
-            hour: 7,
-            isPossible: false,
-        }
-    },
-    {
-        id: 4,
-        day: ' monday',
-        timeSet : {
-            t: 27000,
-            sec: 0,
-            min: 30,
-            hour: 7,
-            isPossible: false,
-        }
-    },
-    {
-        id: 5,
-        day: ' monday',
-        timeSet : {
-            t: 27000,
-            sec: 0,
-            min: 30,
-            hour: 7,
-            isPossible: false,
-        }
-    }
 ];
+
 
 function Timer(){
     const [timeSelector, setTimeSelector] = useState(0);
@@ -127,16 +72,33 @@ function Timer(){
     const [hide, setHide] = useState('none');
     const [time, setTime] = useState({...timeObject});
     const clock = {...timeObject};
-    const alarm = {...timeObject};
+    const [alarm, setAlarm] = useState({...timeObject});
     const [timer, setTimer] = useState({...timeObject});
     const [stopWatch, setStopWatch] = useState({...timeObject});
+
+    
+
+    const alarmRefList = [useRef(), useRef(), useRef(), useRef(), useRef(), useRef()];
 
     const clockHandler = ()=>{
         const date = new Date();
         clock.sec = date.getSeconds();
         clock.min = date.getMinutes();
         clock.hour = date.getHours();
+        alarmHander(date);
     }
+
+    const alarmHander = (date)=>{
+        alarmList.forEach((alarm)=>{
+            if(alarmRefList[alarm.id].current.checked && alarm.day === date.getDay() 
+            && alarm.timeSet.hour === date.getHours() && alarm.timeSet.min === date.getMinutes()){
+                if(date.getSeconds()<10){
+                    alert('timeout!');
+                }   
+            } 
+        });
+    }
+
     const beUnit60 = (tObj)=>{
         let [t, hour, min, sec] = [tObj.t, tObj.hour, tObj.min, tObj.sec];
         hour = parseInt(t / 3600);
@@ -150,7 +112,14 @@ function Timer(){
     const timerHandler = ()=>{
         if(timer.isPossible){
             timer.t -= 1;
-            setTimer(beUnit60(timer));
+
+            console.log(timer.t);
+            if(timer.t <= 0){
+                timer.isPossible = false;
+                setTimer(timeObject);
+            }else {
+                setTimer(beUnit60(timer));
+            }
         }
         
     };
@@ -164,11 +133,11 @@ function Timer(){
     useInterval(() => {
         let tempTime;
         
+        clockHandler();
         timerHandler();
         stopWatchHandler();
         
         if(mode === 'Clock'){
-            clockHandler();
             tempTime = {...clock};
         }else if(mode === 'Alarm'){
             tempTime = {...alarm};
@@ -190,6 +159,7 @@ function Timer(){
             setStyle({
                 'backgroundColor' : btnList[e.target.dataset.id].backgroundColor,
             });
+            setTimeSelector(0);
             
             if(e.target.dataset.id === '4'){
                 setHide('block');
@@ -205,12 +175,16 @@ function Timer(){
                 setTimer({...timeObject, isPossible:false});
             } else if(e.target.className === 'set'){
                 if(!timer.isPossible){
+                    let hour = timeSelector===0 ? timer.hour<23? timer.hour+1: timer.hour :timer.hour;
+                    let min = timeSelector===1 ? timer.min<59 ? timer.min+1: timer.min : timer.min;
+                    let sec = timeSelector===2 ? timer.sec<59 ? timer.sec+1: timer.sec : timer.sec;
+
                     setTimer({
-                        hour: timeSelector===0 ? timer.hour<23? timer.hour+1: timer.hour :timer.hour,
-                        min: timeSelector===1 ? timer.min<59 ? timer.min+1: timer.min : timer.min,
-                        sec: timeSelector===2 ? timer.sec<59 ? timer.sec+1: timer.sec : timer.sec,
+                        hour: hour,
+                        min: min,
+                        sec: sec,
                         isPossible: timer.isPossible,
-                        t: timer.hour * 3600 + timer.min*60 + timer.sec,
+                        t: hour*3600 + min*60 + sec
                     });
                 }
             } else if(e.target.className === 'start'){
@@ -223,7 +197,48 @@ function Timer(){
                 });
             }
         } else if(mode === 'Alarm') {
-            
+            if(e.target.className === 'prev'){
+                setTimeSelector((timeSelector+2)%3);
+            } else if(e.target.className === 'next'){
+                setTimeSelector((timeSelector+1)%3);
+            } else if(e.target.className === 'reset'){
+                alarmList.pop()
+            } else if(e.target.className === 'set'){
+                if(!alarm.isPossible){
+                    let hour = timeSelector===0 ? alarm.hour<23? alarm.hour+1: alarm.hour :alarm.hour;
+                    let min = timeSelector===1 ? alarm.min<59 ? alarm.min+1: alarm.min : alarm.min;
+                    let sec = timeSelector===2 ? alarm.sec<59 ? alarm.sec+1: alarm.sec : alarm.sec;
+
+                    setAlarm({
+                        hour: hour,
+                        min: min,
+                        sec: sec,
+                        isPossible: alarm.isPossible,
+                        t: hour*3600 + min*60 + sec
+                    });
+                }
+            } else if(e.target.className === 'start'){
+                const day = new Date().getDay();
+                
+                if(alarmList.length === 6){
+                    alert('Cannot make anymore!');
+                } else {
+                    
+                    alarmList.push({
+                        id: alarmList.length,
+                        day: day,
+                        timeSet: alarm
+                    });
+
+                    setAlarm({
+                        t: 0,
+                        hour: 0,
+                        min: 0,
+                        sec: 0,
+                        isPossible: false,
+                    });
+                }
+            }
         } else if(mode==='Stop Watch') {
             if(e.target.className === 'reset'){
                 setStopWatch({
@@ -261,11 +276,28 @@ function Timer(){
         <div className="mobile-app">
             <Menu btnList={btnList} style={style} clickEvent={clickHandler}/>
             <Interface mode={mode} style={style} time={time} clickEvent={clickHandler}/>
-            <AlarmInterface alarmSet={alarmList} style={hide}/>
+            <AlarmInterface alarmSet={alarmList} style={hide} refList = {alarmRefList}/>
 
             <Controller clickEvent={clickHandler}/>
         </div>
     );
+}
+
+window.onload = ()=>{
+    const description = `Welcome to Timer Application
+        - Clock: No function, just Clock.
+        - Timer:
+            <, > : move current timeSet
+            timeSet : set up the time of current timeSet
+            start : start the timer
+            reset : reset the timer
+        - Alarm : the same use as timer
+        - Stop Watch : click to start the App
+        - Back Log : manage the Alarm lists
+        `
+
+
+    alert(description);
 }
 
 
